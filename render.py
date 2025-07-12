3a393c2b111dda481b7dcd471a35168fb38528cf
clearmind <touplitoui.pro@gmail.com>
2025-07-12 22:07:23 +0200

    add path spec

--- render.py
+++ render.py
@@ -1,16 +1,16 @@
 import json
 import os
 
-
-with open('full_profile.json', 'r') as fp:
+basedir = os.path.dirname(__file__)
+with open(os.path.join(basedir, 'full_profile.json', 'r')) as fp:
     profile = json.loads(fp.read())
-    with open('profile.template.html', 'r') as pt:
+    with open(os.path.join(basedir, 'profile.template.html', 'r')) as pt:
         data: str = pt.read()
         res = data.format(rank=profile['tier'].lower(), name="Touplitoui", platform="EUW",
                           rankFull=profile['tier'] + " " + profile['rank'], gameNumber=4,
                           champion1='Zoe', nbKills=1, nbDeaths=1, nbAssists=1,kda=1, lp=profile['leaguePoints']
                           )
-        with open('res.html', 'w') as rp:
+        with open(os.path.join(basedir, 'res.html'), 'w') as rp:
             rp.write(res)
 
 
